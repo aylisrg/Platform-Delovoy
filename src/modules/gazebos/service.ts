780030e108a3cffd3026abbc6678d4eba7e725e8
aylisrg <ilya.sergeenko@gmail.com>
2026-04-10 01:24:54 +0400

    auto: update service.ts

--- src/modules/gazebos/service.ts
+++ src/modules/gazebos/service.ts
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/db";
 import type { BookingStatus } from "@prisma/client";
 import type {
   CreateBookingInput,
+  AdminCreateBookingInput,
   CreateResourceInput,
   UpdateResourceInput,
   BookingFilter,
